752babe2a5a42c10ebf19b1c6f8808e903e48cc7
Jinjiang <zhaojinjiang@me.com>
2020-02-09 02:24:49 +0800

    fixed multi-hypermark cases

--- src/rules/mark-hyper.js
+++ src/rules/mark-hyper.js
@@ -1,12 +1,38 @@
-// space besides hyper mark: one or no space outside, no space inside
+// space besides hyper mark:
+// add 1 space outside the most outside mark if there is any space besides mark or mark group
+
+const {
+  findTokenBefore,
+  findTokenAfter,
+  findContentTokenBefore,
+  findContentTokenAfter
+} = require('./util')
+
+const findMarkSeq = (group, token, seq) => {
+  const tokenBefore = findTokenBefore(group, token)
+  const tokenAfter = findTokenAfter(group, token)
+  if (seq.indexOf(tokenBefore) < 0 && tokenBefore.type === 'mark-hyper') {
+    seq.unshift(tokenBefore)
+    findMarkSeq(group, tokenBefore, seq)
+  }
+  if (seq.indexOf(tokenAfter) < 0 && tokenAfter.type === 'mark-hyper') {
+    seq.push(tokenAfter)
+    findMarkSeq(group, tokenAfter, seq)
+  }
+}
 
 module.exports = (token, index, group, matched, marks) => {
   if (token.type === 'mark-hyper') {
-    const tokenBefore = group[index - 1]
-    const tokenAfter = group[index + 1]
+    const markSeq = [token]
+    findMarkSeq(group, token, markSeq)
+
+    const hasSpace = markSeq.some(markToken => markToken.spaceAfter) || (findTokenBefore(group, markSeq[0]) || {}).spaceAfter
+
+    const tokenBefore = findTokenBefore(group, token)
+    const tokenAfter = findTokenAfter(group, token)
     if (token.markSide === 'left') {
       if (tokenBefore) {
-        tokenBefore.spaceAfter = ' '
+        tokenBefore.spaceAfter = (hasSpace && token === markSeq[0]) ? ' ' : ''
       }
       if (tokenAfter) {
         token.spaceAfter = ''
@@ -17,7 +43,7 @@ module.exports = (token, index, group, matched, marks) => {
         tokenBefore.spaceAfter = ''
       }
       if (tokenAfter) {
-        token.spaceAfter = ' '
+        token.spaceAfter = (hasSpace && token === markSeq[markSeq.length - 1]) ? ' ' : ''
       }
     }
   }
