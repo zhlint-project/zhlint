1287a4b5a642b9bf81f2969b57c830d72117699d
Jinjiang <zhaojinjiang@me.com>
2020-02-09 02:32:11 +0800

    supported full-width-content with hyper marks

--- src/rules/space-full-width-content.js
+++ src/rules/space-full-width-content.js
@@ -1,10 +1,3 @@
-// todo:
-// - nextToken -> nextNonHyperMarkToken
-// - if content width different
-//   - if there is a mark
-//     - add a space outside mark
-//   - else
-//     - add a space between
 // examples:
 // - *a*啊 -> *a*啊 -> *a* 啊
 // - *a* 啊 -> *a* 啊
@@ -19,23 +12,38 @@
 // - 啊 *a* -> 啊 *a*
 // - 啊* a* -> 啊 *a*
 
+const {
+  findTokenBefore,
+  findTokenAfter,
+  findContentTokenBefore,
+  findContentTokenAfter,
+  findMarkSeq
+} = require('./util')
+
 module.exports = (token, index, group, matched, marks) => {
-  // token is content
-  // next token exists and is content too
-  // one is half-width another is full-width
-  // !token.spaceAfter or token.spaceAfter.length > 1
+  // - if next content width different
+  //   - if there is a mark
+  //     - add a space outside mark
+  //   - else
+  //     - add a space between
   if (!token.type.match(/^content\-/)) {
     return
   }
-  const nextToken = group[index + 1]
-  if (!nextToken) {
+  const contentTokenAfter = findContentTokenAfter(group, token)
+  if (!contentTokenAfter) {
     return
   }
-  if (!nextToken.type.match(/^content\-/)) {
+  if (contentTokenAfter.type === token.type) {
     return
   }
-  if (nextToken.type === token.type) {
-    return
+  const tokenAfter = findTokenAfter(group, token)
+  if (tokenAfter === contentTokenAfter) {
+    token.spaceAfter = ' '
+  } else {
+    if (tokenAfter.markSide === 'left') {
+      token.spaceAfter = ' '
+    } else {
+      findTokenBefore(contentTokenAfter).spaceAfter = ' '
+    }
   }
-  token.spaceAfter = ' '
 }
