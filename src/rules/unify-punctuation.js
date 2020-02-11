12cea6a2a83f9721fa4b6df8fda26cd92cb20b2f
Jinjiang <zhaojinjiang@me.com>
2020-02-12 01:54:49 +0800

    fixed dot in URL bug

--- src/rules/unify-punctuation.js
+++ src/rules/unify-punctuation.js
@@ -1,3 +1,9 @@
+const {
+  findTokenBefore,
+  findContentTokenBefore,
+  findContentTokenAfter
+} = require('./util')
+
 const halfWidthMap = {
   '（': `(`,
   '）': `)`,
@@ -15,8 +21,20 @@ const fullWidthMap = {
 module.exports = (token, index, group, matched, marks) => {
   // full-width: comma, full stop, colon, quotes
   // half-width: brackets
-  // todo: special cases? dot dot dot, hyphen hyphen?
+  // no change for half-width puncatuation between half-width content without space
   if (token.type === 'punctuation-half') {
+    const contentTokenBefore = findContentTokenBefore(group, token)
+    const contentTokenAfter = findContentTokenAfter(group, token)
+    const tokenBefore = findTokenBefore(group, token)
+    const tokenBeforeContentTokenAfter = findTokenBefore(group, contentTokenAfter)
+    if (contentTokenBefore && contentTokenBefore.type === 'content-half'
+      && contentTokenAfter && contentTokenAfter.type === 'content-half'
+      && !contentTokenBefore.spaceAfter
+      && !tokenBefore.spaceAfter
+      && !token.spaceAfter
+      && !tokenBeforeContentTokenAfter.spaceAfter) {
+      return
+    }
     token.content = fullWidthMap[token.content] || token.content
   }
   if (token.type === 'punctuation-width') {
