459fc6d2b2b4da704d0da692a2f9d305a17291a4
Jinjiang <zhaojinjiang@me.com>
2020-10-05 01:27:08 +0800

    fix: small code format issue in hexo parser

--- src/parsers/hexo.js
+++ src/parsers/hexo.js
@@ -1,20 +1,20 @@
 // {% x y %}z{% endx %}
-// \{\% ([^ ]+?) [^\%]+?\%\}    ([^ ]+?)        [^\%]*?
+// \{\% ([^ ]+?) [^\%]*?\%\}    ([^ ]+?)        [^\%]*?
 // (?:\n|\{(?!\%)|[^\{])*?      \n              \{(?!\%)        [^\{]
 // \{\% end(?:\1) \%\}
 const matcher = /\{\% ([^ ]+?) [^\%]*?\%\}(?:\n|\{(?!\%)|[^\{])*?\{\% end(?:\1) \%\}/g
 
 module.exports = data => {
   data.content = data.content.replace(matcher, (raw, name, index ) => {
-      const { length } = raw
-      data.ignoredByParsers.push({
-        name,
-        index,
-        length,
-        raw,
-        meta: `hexo-${name}`
-      })
-      return '@'.repeat(length)
+    const { length } = raw
+    data.ignoredByParsers.push({
+      name,
+      index,
+      length,
+      raw,
+      meta: `hexo-${name}`
+    })
+    return '@'.repeat(length)
   })
   return data
 }
