0940646d087c8374bfc8c0a1b815edeac9aa52bf
Jinjiang <zhaojinjiang@me.com>
2022-04-26 14:49:19 +0800

    test: hyper-space-option rule

--- test/rules.test.ts
+++ test/rules.test.ts
@@ -5,14 +5,46 @@ import run, { Options } from '../src/run'
 const lint = (...args: [string, Options?]) => run(...args).result
 
 describe('lint by rule', () => {
-  test('[hyper-space-option] the existence of spaces around hyper marks', () => {
-    expect(lint('xxx`foo` xxx',
-      { rules: { hyper: { codeSpace: true }}}
-    )).toBe('xxx `foo` xxx')
-    expect(lint('xxx `foo`xxx',
-      { rules: { hyper: { codeSpace: false }}}
-    )).toBe('xxx`foo`xxx')
-    expect(lint('xxx `foo`xxx')).toBe('xxx `foo`xxx')
+  describe('[hyper-space-option] the existence of spaces around hyper marks', () => {
+    test('forcing spaces', () => {
+      const options = { rules: { hyper: { codeSpace: true }}}
+      expect(lint('xxx`foo`xxx', options)).toBe('xxx `foo` xxx')
+      expect(lint('xxx`foo` xxx', options)).toBe('xxx `foo` xxx')
+      expect(lint('xxx `foo`xxx', options)).toBe('xxx `foo` xxx')
+      expect(lint('xxx `foo` xxx', options)).toBe('xxx `foo` xxx')
+
+      // internal spaces would be kept
+      expect(lint('xxx ` foo`xxx', options)).toBe('xxx ` foo` xxx')
+
+      expect(lint('xxx<code>foo</code>xxx', options)).toBe('xxx <code>foo</code> xxx')
+      expect(lint('xxx<code>foo</code> xxx', options)).toBe('xxx <code>foo</code> xxx')
+      expect(lint('xxx <code>foo</code>xxx', options)).toBe('xxx <code>foo</code> xxx')
+      expect(lint('xxx <code>foo</code> xxx', options)).toBe('xxx <code>foo</code> xxx')
+    })
+    test('forcing no spaces', () => {
+      const options = { rules: { hyper: { codeSpace: false }}}
+      expect(lint('xxx`foo`xxx', options)).toBe('xxx`foo`xxx')
+      expect(lint('xxx`foo` xxx', options)).toBe('xxx`foo`xxx')
+      expect(lint('xxx `foo`xxx', options)).toBe('xxx`foo`xxx')
+      expect(lint('xxx `foo` xxx', options)).toBe('xxx`foo`xxx')
+
+      expect(lint('xxx<code>foo</code>xxx', options)).toBe('xxx<code>foo</code>xxx')
+      expect(lint('xxx<code>foo</code> xxx', options)).toBe('xxx<code>foo</code>xxx')
+      expect(lint('xxx <code>foo</code>xxx', options)).toBe('xxx<code>foo</code>xxx')
+      expect(lint('xxx <code>foo</code> xxx', options)).toBe('xxx<code>foo</code>xxx')
+    })
+    test('keep the original spaces', () => {
+      const options = undefined
+      expect(lint('xxx`foo`xxx', options)).toBe('xxx`foo`xxx')
+      expect(lint('xxx`foo` xxx', options)).toBe('xxx`foo` xxx')
+      expect(lint('xxx `foo`xxx', options)).toBe('xxx `foo`xxx')
+      expect(lint('xxx `foo` xxx', options)).toBe('xxx `foo` xxx')
+
+      expect(lint('xxx<code>foo</code>xxx', options)).toBe('xxx<code>foo</code>xxx')
+      expect(lint('xxx<code>foo</code> xxx', options)).toBe('xxx<code>foo</code> xxx')
+      expect(lint('xxx <code>foo</code>xxx', options)).toBe('xxx <code>foo</code>xxx')
+      expect(lint('xxx <code>foo</code> xxx', options)).toBe('xxx <code>foo</code> xxx')
+    })
   })
   test('[hyper-space-position] the position of spaces around hyper marks (if any)', () => {
     expect(lint('1', {
