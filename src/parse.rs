de6b452a6febf9d519dc93e76452a316e7b43ce0
Jinjiang <zhaojinjiang@me.com>
2024-08-01 21:00:56 +0800

    main space handling in parse

--- src/parse.rs
+++ src/parse.rs
@@ -117,7 +117,7 @@ fn create_mark(
 #[allow(dead_code)]
 fn add_bracket_token() {}
 
-#[allow(dead_code, unused_variables)]
+#[allow(unused_variables)]
 fn finalize_last_token(
   status: &ParseStatus,
   index: usize
@@ -165,13 +165,17 @@ fn create_content() {}
 #[allow(dead_code)]
 fn append_value() {}
 
-#[allow(dead_code)]
-fn get_space_length() {}
+#[allow(unused_variables, dead_code)]
+fn get_space_length(str: &str, i: usize) -> usize {
+  1
+}
 
-#[allow(dead_code)]
-fn get_prev_token() {}
+#[allow(unused_variables)]
+fn get_prev_token(status: &ParseStatus) -> Option<&Rc<RefCell<Token>>> {
+  None
+}
 
-#[allow(dead_code, unused_variables)]
+#[allow(unused_variables)]
 fn is_shorthand(
   str: &str,
   status: &ParseStatus,
@@ -184,13 +188,52 @@ fn is_shorthand(
 #[allow(dead_code)]
 fn get_hyper_content_type() {}
 
+fn temp_add_spaces(token: &Rc<RefCell<Token>>, spaces: &str) {
+  match &mut *token.borrow_mut() {
+    Token::SingleToken(single_token) => {
+      single_token.token.space_after = String::from(spaces);
+    },
+    Token::GroupToken(group_token) => {
+      group_token.token.space_after = String::from(spaces);
+    },
+  }
+}
+
 pub fn parse(str: &str) -> ParseResult {
   let status = create_status(str);
+
+  let mut last_index = 0;
   for (i, c) in str.chars().enumerate() {
+    // skip already handled spaces
+    if i < last_index {
+      continue;
+    }
+    last_index = i;
+
+    // get char type
     let char_type = get_char_type(c);
+
     // if char_type is hyper!
     if char_type == CharType::Space {
-      //
+      finalize_last_token(&status, i);
+      let last_group = status.last_group.as_ref();
+      if last_group.is_some() {
+        let space_len = get_space_length(str, i);
+        let spaces = &str[i..i + space_len];
+        if last_group.unwrap().borrow().children.len() > 0 {
+          let last_token = get_prev_token(&status);
+          if last_token.is_some() {
+            let last_token_value = last_token.unwrap();
+            temp_add_spaces(last_token_value, spaces);
+          }
+        } else {
+          let last_group_value = last_group.unwrap();
+          last_group_value.borrow_mut().inner_space_before = String::from(spaces);
+        }
+        if space_len > 1 {
+          last_index = i + space_len - 1;
+        }
+      }
     } else if is_shorthand(str, &status, i, c) {
       //
     } else if char_type.is_punctuation() {
