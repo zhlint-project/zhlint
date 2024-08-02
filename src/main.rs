72aca23a35c937ae1e109cde50d8cd56ccd12818
Jinjiang <zhaojinjiang@me.com>
2024-08-02 23:40:50 +0800

    remove learning files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// use char_type::get_char_type;
-// use learn_borrow::foo;
 use parse::parse;
 
 pub mod char_type;
@@ -7,12 +5,8 @@ pub mod token_type;
 pub mod type_trait;
 pub mod parse;
 pub mod parse_util;
-pub mod learn_borrow;
 
 fn main() {
-  // let a = get_char_type(' ');
-  // println!("Hello, world! {:?}", a);
-  // foo();
   let str = "中文，English 中文";
   println!("[Input]\n{:?}", str);
   let result = parse(str);
