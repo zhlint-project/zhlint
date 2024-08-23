a088c4c77195f9e2e92314e66cf5faad9b7c34b8
Jinjiang <zhaojinjiang@me.com>
2024-08-23 20:07:12 +0800

    add a test case to modify MutParseResult

--- src/generator.rs
+++ src/generator.rs
@@ -6,16 +6,33 @@ pub fn generate(result: &MutParseResult) -> String {
 
 #[cfg(test)]
 mod tests {
-  use crate::parse;
+  use crate::{parse, token_type::MutTokenExtraType};
   use super::*;
+
+  fn modify_result(result: &mut MutParseResult) {
+    if let MutTokenExtraType::Group(
+      e,
+      ee
+    ) = &mut result.root.extra {
+      if let Some(second) = e.children.get_mut(1) {
+        if let MutTokenExtraType::Single(second_extra) = &mut second.extra {
+          second_extra.modified_value = String::from(",");
+          second_extra.modified_space_after = String::from(" ");
+        }
+      }
+      ee.modified_start_value = String::from("!!");
+      ee.modified_end_value = String::from("!!");
+    }
+  }
 
   #[test]
   fn test_parse() {
     let str = "中文，English 中文";
     let result = parse(str);
     println!("{:#?}", result);
-    let mut_result = result.to_mut();
+    let mut mut_result = result.to_mut();
     println!("{:#?}", mut_result);
+    modify_result(&mut mut_result);
     let generated = generate(&mut_result);
     println!("{}", generated);
   }
