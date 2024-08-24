339cae63bcd649b0c60295a5b3430bbf08b0eaa6
Jinjiang <zhaojinjiang@me.com>
2024-08-25 02:05:13 +0800

    try traverse()

--- src/lint.rs
+++ src/lint.rs
@@ -1,4 +1,6 @@
-pub fn lint() {
+use crate::token::token_type::MutToken;
+
+pub fn lint(_token: &mut MutToken, _index: usize, _parent: &MutToken) {
   // TODO:
   println!("Linting...");
 }
--- src/traverse.rs
+++ src/traverse.rs
@@ -1,5 +1,18 @@
-use crate::parse::context::MutParseResult;
+use crate::{parse::context::MutParseResult, token::token_type::{MutToken, MutTokenExtraType}};
 
-pub fn traverse(_tokens: &mut MutParseResult, _lint: fn()) {
-  // TODO:
+pub fn traverse(data: &mut MutParseResult, lint: fn(&mut MutToken, usize, &MutToken)) {
+  iterate(&mut data.root, lint)
+}
+
+fn iterate(token: &mut MutToken, _lint: fn(&mut MutToken, usize, &MutToken)) {  
+  match &mut token.extra {
+    MutTokenExtraType::Group(extra, _mut_extra) => {
+      extra.children.iter_mut().enumerate().for_each(|(_index, _child)| {
+        // TODO:
+        // lint(child, index, token);
+        // iterate(child, lint);
+      });
+    },
+    _ => {},
+  }
 }
